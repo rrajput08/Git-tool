2fd4c7e339417fbceb6edeb6776b090808365b6f
rrajput08 <roshanirajput99@gmail.com>
2023-11-27 21:53:41 +0530

    compo

--- myApp/src/app/app-routing.module.ts
+++ myApp/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path : '' , component : HomeComponent},
+{ path : "login" , component : LoginComponent},
+{path : "dataBinding", component : DatabindingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
