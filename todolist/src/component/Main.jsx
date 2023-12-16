aa8c815a463741c737a990c9e1c58b7ff5c64d71
rrajput08 <roshanirajput99@gmail.com>
2023-12-16 23:14:28 +0530

    changes

---
+++ todolist/src/component/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      <h1 className='heading1_main'>I am James Anderson</h1>
+        <h1>And i am a web developer</h1>
+        <h5>this is from  
+          <span style={{color:"cyan"}}>colorlib</span>
+        </h5>
+    </div>
+  )
+}
+
+export default Main
